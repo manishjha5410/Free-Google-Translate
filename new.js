9cfc8cf463cb23ba998591a651cb9f645a3dccc9
manishjha5410 <manishjha5410@gmail.com>
2021-03-29 16:44:03 +0530

    Styles Modified

--- new.js
+++ new.js
@@ -4,6 +4,8 @@ dom.setAttribute('src',"https://translate.google.com/translate_a/element.js?cb=g
 
 document.querySelector("head").append(dom);
 
+let languages = [];
+
 function googleTranslateElementInit() {
 	new google.translate.TranslateElement(
 		{pageLanguage: 'en'},
@@ -13,15 +15,14 @@ function googleTranslateElementInit() {
     for(var i=1;i<3;i++)
         document.querySelector(".skiptranslate.goog-te-gadget").childNodes[1].remove();
 
-    console.log(document.querySelector(".goog-te-combo").childElementCount);
-
-
-    document.querySelector(".goog-te-combo").onclick = () =>{
-        document.querySelector(".goog-te-combo").childNodes.forEach((key, value) =>{
-            console.log(key,value);
-        })
-    }
+    document.querySelector(".goog-te-combo").classList.add("form-select");
 
     document.querySelector("div#goog-gt-tt").remove();
+    document.querySelector(".goog-te-spinner-pos").remove();
 }
 
+document.onmouseover = function(){
+    if(document.querySelector(".goog-te-menu-frame")!==null){
+        document.querySelectorAll(".goog-te-menu-frame").forEach(key=>key.remove());
+    }
+}
